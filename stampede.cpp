46b6cea8b7b375c33e57993a1288cef10077f7b2
stampede-arm <c.cromwell@lamarschools.org>
2017-09-24 18:05:37 -0600

    Stepper Motor Functionality
    This commit marks a HUGE improvement to the library: Stepper Motor Functionality.
    The same sketch as before can be used, and a new bluetooth keyword, stepperX, has been added. (where x is the stepper number.) So far stepper definitions occour in the library, so there is still improvement to the library to be made. Expects ULN2003 based stepper connected to 32,31,30,29 on the Teensy.

--- stampede.cpp
+++ stampede.cpp
@@ -7,6 +7,58 @@
 #include "Arduino.h"
 #include <Servo.h>
 #include "stampede.h"
+StepperMotor::StepperMotor(int In1, int In2, int In3, int In4){
+    // Record pin numbers in the inputPins array
+    this->inputPins[0] = In1;
+    this->inputPins[1] = In2;
+    this->inputPins[2] = In3;
+    this->inputPins[3] = In4;
+
+    // Iterate through the inputPins array, setting each one to output mode
+    for(int inputCount = 0; inputCount < 4; inputCount++){
+        pinMode(this->inputPins[inputCount], OUTPUT);
+    }
+    duration = 50;
+}
+
+void StepperMotor::setStepDuration(int duration){
+    this->duration = duration;
+}
+
+void StepperMotor::step(int noOfSteps){
+    /*
+        The following 2D array represents the sequence that must be
+        used to acheive rotation. The rows correspond to each step, and
+        the columns correspond to each input. L
+    */
+    bool sequence[][4] = {{LOW, LOW, LOW, HIGH },
+                          {LOW, LOW, HIGH, HIGH},
+                          {LOW, LOW, HIGH, LOW },
+                          {LOW, HIGH, HIGH, LOW},
+                          {LOW, HIGH, LOW, LOW },
+                          {HIGH, HIGH, LOW, LOW},
+                          {HIGH, LOW, LOW, LOW },
+                          {HIGH, LOW, LOW, HIGH}};
+                      
+    int factor = abs(noOfSteps) / noOfSteps;    // If noOfSteps is +, factor = 1. If noOfSteps is -, factor = -1 
+    noOfSteps = abs(noOfSteps);    // If noOfSteps was in fact negative, make positive for future operations
+
+    /* 
+        The following algorithm runs through the sequence the specified number 
+        of times
+    */
+    for(int sequenceNum = 0;  sequenceNum <= noOfSteps/8; sequenceNum++){
+        for(int position = 0; ( position < 8 ) && ( position < ( noOfSteps - sequenceNum*8 )); position++){
+            delay(duration);
+            for(int inputCount = 0; inputCount < 4; inputCount++){
+                digitalWrite(this->inputPins[inputCount], sequence[(int)(3.5 - (3.5*factor) + (factor*position))][inputCount]);
+            }
+        } 
+    }
+}
+void StepperMotor::degree(int noOfDeg) {
+step((noOfDeg*4096)/360);
+}
 // Constructor
 arm::arm()
 {
@@ -19,6 +71,7 @@ char messageForArm[32] = {0};
 int intForArm = 0.0;
 boolean newData = false;
 String messageForArmStr;
+StepperMotor motor(32,31,30,29);
 }
 void arm::setupfingers()
 {
@@ -40,6 +93,7 @@ void arm::fingerattach(int finger1pin, int finger2pin, int finger3pin, int finge
     finger3.attach(finger3pin);
     finger4.attach(finger4pin);
     finger5.attach(finger5pin);
+    motor.setStepDuration(1);
 }
 
 void arm::fingermove(int finger, int fingermovevalue)
@@ -248,5 +302,12 @@ void arm::btshowParsedData() {
     bt.print(intForArm);
 	bt.println("'");
 	wiggle(180);
+  } else if (messageForArmStr == "stepper1") {
+    bt.print("The message was '");
+    bt.print(messageForArm);
+    bt.print("' and the int was '");
+    bt.print(intForArm);
+	bt.println("'");
+	motor.degree(intForArm);
   }
 }
--- stampede.h
+++ stampede.h
@@ -10,6 +10,16 @@
 
 #include "Arduino.h"
 #include <servo.h>
+class StepperMotor {
+public:
+    StepperMotor(int In1, int In2, int In3, int In4);    // Constructor that will set the inputs
+    void setStepDuration(int duration);    // Function used to set the step duration in ms
+    void step(int noOfSteps);    // Step a certain number of steps. + for one way and - for the other
+	void degree(int noOfDeg);
+    int duration;    // Step duration in ms
+    int inputPins[4];    // The input pin numbers
+};
+
 class arm {
 public:
     int i;
@@ -44,5 +54,6 @@ public:
 	void btrecvWithStartEndMarkers();
 	void btparseData();
 	void btshowParsedData();
+  StepperMotor motor = StepperMotor(32,31,30,29);
 };
 #endif
